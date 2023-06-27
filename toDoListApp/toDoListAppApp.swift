cc327420269069a33fd843ee6e2605954e717349
Emaiya James-Plew <emaiya.jp@gmail.com>
2023-06-27 10:45:11 -0600

    Initial Commit

---
+++ toDoListApp/toDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListAppApp.swift
+//  toDoListApp
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
